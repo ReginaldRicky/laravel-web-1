da7c4f24d1d43973e3de56b72b0365bcf6887786
Reginald <reginald24ti@mahasiswa.pcr.ac.id>
2025-12-01 14:47:45 +0700

    coba2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,13 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
+    })
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' =>
+    \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
+
     })->create();
